7d52c40c6525864bfcd63bbbc846f5f55ba67d0f
Nikita Kononenko <vmeansdev@gmail.com>
2017-10-03 13:41:25 +0300

    UPDATE: task 1.7

---
+++ module_1/mlist.erl
@@ -0,0 +1,73 @@
+%% - task 1.7
+-module(mlist).
+-export([create/1, reverse_create/1,
+         filter/2, reverse/1, concatenate/1,
+         flatten/1, dna_to_rna/1]).
+
+
+%% - Напишите функцию create/1, которая на вход принимает число N
+%% - и возвращает список вида [1, 2,..., N -1, N].
+create(0) -> [];
+create(N) when N > 0 ->
+  create(N - 1) ++ [N].
+
+%% - Напишите функцию, которая также принимает число N, но возвращает список вида [N, N-1, ..., 2,1].
+reverse_create(0) -> create(0);
+reverse_create(N) when N > 0 ->
+  [N | create(N - 1)].
+
+%% - Напишите функцию, которая распечатывает все числа от 1 до N.
+%% - Напишите функцию, которая распечатывает все нечётные числа от 1 до N.
+
+%% - Напишите функцию, которая принимает на вход список целых чисел и одно целое число,
+%% - а возвращает список всех элементов списка, которые меньше либо равны числу, переданному вторым аргументом. Пример: filter([1, 2, 3, 4, 5], 3) => [1,2,3].
+filter(Acc, [H|T], Num) when H =< Num ->
+  filter(Acc ++ [H], T, Num);
+filter(Acc, [H|T], Num) when H >= Num ->
+  filter(Acc, T, Num);
+filter(Acc, [], _Num) -> Acc.
+
+filter(List, Num) ->
+  filter([], List, Num).
+
+%% - Напишите функцию, которая переворачивает список. Пример: reverse([1,2,3]) => [3,2,1].
+reverse(Acc, [H | T]) -> reverse([H | Acc], T);
+reverse(Acc, []) -> Acc.
+reverse([]) -> [];
+reverse(List) ->
+  reverse([], List).
+
+%% - Напишите функцию, которая преобразует список списков в один список,
+%% - соединяя все списки-элементы. Пример: concatenate([[1,2,3], [], [4, five]]) => [1,2,3,4,five].
+concatenate(Acc, []) -> Acc;
+concatenate(Acc, [H | T]) when is_list(H) ->
+  concatenate(Acc, H ++ T);
+concatenate(Acc, [H | T]) ->
+  concatenate(Acc ++ [H], T).
+
+concatenate(List) ->
+  concatenate([], List).
+
+%% - Напишите функцию, которая по списку вложенных списков
+%% - строит линейный список. Пример: flatten([[1, [2 , [3] , [] ], [[[4]]], [5,6]]) => [1,2,3,4,5,6].
+flatten(List) -> concatenate(List).
+
+%% - Напишите функцию, которая принимает на вход последовательность нуклеотидов ДНК и выдает
+%% - комплементарную ей цепочку нуклеотидов РНК. Траскрипция происходит путем замены нуклеотидов
+%% - в исходной цепочке ДНК на парные им нуклеотиды, входящие в цепочку РНК. ДНК содержит четыре нуклеотида:
+%% - A - аденин, С - цитозин, G - гуанин, T - тимин. В РНК содержатся следующие нуклеотиды:
+%% - A - аденин, С - цитозин, G - гуанин, U - урацил.
+dna_to_rna(Acc, []) -> Acc;
+dna_to_rna(Acc, [H|T]) when H == $G; H == $g; H == g ->
+  dna_to_rna(Acc ++ [$c], T);
+dna_to_rna(Acc, [H|T]) when H == $C; H == $c; H == c ->
+  dna_to_rna(Acc ++ [$g], T);
+dna_to_rna(Acc, [H|T]) when H == $T; H == $t; H == t ->
+  dna_to_rna(Acc ++ [$a], T);
+dna_to_rna(Acc, [H|T]) when H == $A; H == $a; H == a ->
+  dna_to_rna(Acc ++ [$u], T).
+
+dna_to_rna([]) -> [];
+
+dna_to_rna(List) when is_list(List) ->
+  dna_to_rna([], List).
