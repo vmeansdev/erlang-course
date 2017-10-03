75c02441eed6e12b30a2405aeadb5704de85394d
Nikita Kononenko <vmeansdev@gmail.com>
2017-10-03 14:41:08 +0300

    UPDATE: task 1.7 additions

--- module_1/mlist.erl
+++ module_1/mlist.erl
@@ -1,8 +1,9 @@
 %% - task 1.7
 -module(mlist).
--export([create/1, reverse_create/1,
-         filter/2, reverse/1, concatenate/1,
-         flatten/1, dna_to_rna/1]).
+-export([create/1,      reverse_create/1, print/1,
+         print_odd/1,   filter/2,         reverse/1,
+         concatenate/1, flatten/1,        dna_to_rna/1,
+         cut_rdna/2]).
 
 
 %% - Напишите функцию create/1, которая на вход принимает число N
@@ -17,7 +18,35 @@ reverse_create(N) when N > 0 ->
   [N | create(N - 1)].
 
 %% - Напишите функцию, которая распечатывает все числа от 1 до N.
+print(To, From) when To > From ->
+  io:format("~p~n", [From]),
+  Iter = From + 1,
+  print(To, Iter);
+print(To,From) when To == From ->
+  io:format("~p~n", [From]).
+
+print(N) when N < 0 -> false;
+print(0) -> 0;
+print(N) ->
+  print(N, 1).
+
 %% - Напишите функцию, которая распечатывает все нечётные числа от 1 до N.
+print_odd(To,From) when To > From, From rem 2 == 0 ->
+  print_odd(To, From + 1);
+print_odd(To,From) when To == From, From rem 2 == 0 ->
+  ok;
+
+print_odd(To, From) when To > From, From rem 2 =/= 0 ->
+  io:format("~p~n", [From]),
+  Iter = From + 1,
+  print_odd(To, Iter);
+print_odd(To, From) when To == From, From rem 2 =/= 0 ->
+  io:format("~p~n", [From]).
+
+print_odd(N) when N < 0 -> false;
+print_odd(0) -> 0;
+print_odd(N) ->
+  print_odd(N, 1).
 
 %% - Напишите функцию, которая принимает на вход список целых чисел и одно целое число,
 %% - а возвращает список всех элементов списка, которые меньше либо равны числу, переданному вторым аргументом. Пример: filter([1, 2, 3, 4, 5], 3) => [1,2,3].
@@ -71,3 +100,11 @@ dna_to_rna([]) -> [];
 
 dna_to_rna(List) when is_list(List) ->
   dna_to_rna([], List).
+
+%% - Напишите функцию, которая из заданной цепочки РНК/ДНК вырезает
+%% - заданную последовательность из трех нуклеотидов.
+%% - Пример: cut_rdna("AAGGTT", "AGG") => "ATT".
+cut_rdna([], _) -> [];
+cut_rdna(List, []) -> List;
+cut_rdna(List, Seq) when length(Seq) == 3 -> %% just stub now
+  List.
